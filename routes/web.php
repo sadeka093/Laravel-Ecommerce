a0220f74520cefed18820cb33c4ac6e883a306d2
sadeka <sadeka@technbit.com>
2021-03-14 13:30:18 -0800

    login done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view("/","login");
+Route::get('/login',function()
+{
+	return view('login');
+});
+
+Route::post("/login",[UserController::class,'login']);
+Route::get("/",[ProductController::class,'index']);
