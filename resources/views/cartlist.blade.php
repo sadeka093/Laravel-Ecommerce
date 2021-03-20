4ad835c3f7b2bda9f7d6131b5a390ab5ab22df68
sadeka <sadeka@technbit.com>
2021-03-20 15:50:03 -0800

    remove from cart

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -27,7 +27,7 @@
 
 
       <div class="col-sm-3">
-         <button class="btn btn-warning">Remove From Cart</button>
+         <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning">Remove From Cart</a>
       </div>
 
 
